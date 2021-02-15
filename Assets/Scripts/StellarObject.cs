b09c08977844370cee6eb6dd71c32ef000789eec
Zachary Cusson <zachcv@gmail.com>
2021-02-15 12:39:43 -0500

    troubleshooting et tests

--- Assets/Scripts/StellarObject.cs
+++ Assets/Scripts/StellarObject.cs
@@ -5,16 +5,48 @@ using UnityEngine;
 public class StellarObject : MonoBehaviour
 {
     private GameManager gameManager;
-    private Vector3 velocity;
-    private float volume; // en m ou km
-    private float mass;
-    private float density;
+    [SerializeField]private Vector3 velocity;
+    [SerializeField]private float volume; // en m ou km
+    [SerializeField] private float mass;
+    [SerializeField]private float density;
    
     //Ajuste automatiquement les valeurs des variables lors du changement d<une
-    public float Density { get => density; set { density = value; volume = mass / density; } }
-    public float Mass { get => mass;  set { mass = value; density = mass / volume; volume = mass / density; }} 
-    public float Volume { get => volume; set { volume = value; density = mass / volume; }  }
+    public float Density 
+    { 
+        get => density; 
+        set 
+        { 
+            density = value; 
+            volume = mass / density; 
+        } 
+    }
+    public float Mass 
+    { 
+        get => mass;  
+        set 
+        { 
+            mass = value; 
+            density = mass / volume; 
+            volume = mass / density; 
+        }
+    }
     public float Radius => Mathf.Pow((volume / 4.1889f), (1 / 3f));
+    public float Volume 
+    { 
+        get => volume; 
+        set 
+        { 
+            volume = value; 
+            density = mass / volume;
+            transform.localScale = new Vector3(Radius, Radius, Radius);
+        }  
+    }
+   
+    public Vector3 Velocity 
+    { 
+        get => velocity; 
+        set => velocity = value; 
+    }
 
     void Start()
     {
@@ -41,12 +73,12 @@ public class StellarObject : MonoBehaviour
     public void ApplyForce(Vector3 force, float time)
     {
         //Vector3 acc = force / Mass;
-        velocity += (force / Mass) * time;
+        Velocity += (force / Mass) * time;
     }
 
     //applique un vecteur force a partir dun objet stellaire
     public void ApplyVelocity(float time)
     {
-        transform.position = velocity * time;
+        transform.position += Velocity * time;
     }
 }
