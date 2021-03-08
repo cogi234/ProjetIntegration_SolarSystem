2383c7c013a4436da5943af684318aad47a51414
Zachary Cusson <zachcv@gmail.com>
2021-03-08 12:34:44 -0500

    presets

--- Assets/Scripts/UI/UITimeDisplay.cs
+++ Assets/Scripts/UI/UITimeDisplay.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class UITimeDisplay : MonoBehaviour
 {
     private Text textBox;
+    private GameManager gameManager;
 
     private void Start()
     {
         textBox = GetComponent<Text>();
+
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
 
     private void Update()
     {
         //Comme PlaceHolder, je met le temps de Time. On va changer ca avec les outils de controle de temps
-        float time = Time.time;
-        int year = Mathf.FloorToInt(time / 31536000);//31536000 secondes par an
+        double time = gameManager.absoluteTime;
+        int year = (int)Math.Floor(time / 31536000);//31536000 secondes par an
         time -= year * 31536000;
-        int day = Mathf.FloorToInt(time / 86400);//86400 secondes par jour
+        int day = (int)Math.Floor(time / 86400);//86400 secondes par jour
         time -= day * 86400;
-        int hour = Mathf.FloorToInt(time / 3600);//3600 secondes par heure
+        int hour = (int)Math.Floor(time / 3600);//3600 secondes par heure
         time -= hour * 3600;
-        int minute = Mathf.FloorToInt(time / 60);//60secondes par minute
+        int minute = (int)Math.Floor(time / 60);//60secondes par minute
         time -= minute * 60;
 
-        textBox.text = $"Time: \n {year}y {day}d {hour}h {minute}m {Mathf.FloorToInt(time)}s";
+        textBox.text = $"Time: \n {year}y {day}d {hour}h {minute}m {(int)Math.Floor(time)}s";
     }
 }
