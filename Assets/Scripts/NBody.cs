76a8b6582182d5caae4ae3799d476d493f503fd7
Zachary Cusson <zachcv@gmail.com>
2021-03-15 12:33:24 -0400

    Debut nbody

---
+++ Assets/Scripts/NBody.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class N-body : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // A(Xn) = G * m * r / abs(r)pow3
+    // acc (objet Xn) = ConstGrav * MassObjetStellaire * distanceObjet
+    //                    / abs(distanceObjetStellaire)pow3
+    float acceleration = 
+
+    // Vn+1 = Vn + h * An
+    // h = delta temps
+
+    // Xn+1 = Xn + h * Vn+1
+
+
+    
+}
