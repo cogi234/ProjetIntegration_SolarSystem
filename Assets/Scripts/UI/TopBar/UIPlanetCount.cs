5ea430e029f38d81caa5411980bad981de524238
Colin <colinbougie@gmail.com>
2021-05-03 11:30:40 -0400

    Planet Count

---
+++ Assets/Scripts/UI/TopBar/UIPlanetCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlanetCount : MonoBehaviour
+{
+    Text text;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();//We find the gameManager
+    }
+
+    private void Update()
+    {
+        text.text = $"Planet Count:\n {gameManager.stellarObjectList.Count}";
+    }
+}
